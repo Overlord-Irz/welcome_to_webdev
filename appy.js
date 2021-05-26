5c4ecc98b30c6e9d2b6abcb83cb1fa84028365ce
Overlord-Irz <74696105+Overlord-Irz@users.noreply.github.com>
2021-05-25 21:32:28 -0400

    Add files via upload

---
+++ appy.js
@@ -0,0 +1,7 @@
+console.log('This is Appyy!');
+function myFunction(){
+     var text1 = "Hello!";
+     var text2 = "Happy people!";
+     var text3 = text1 + text2;
+     document.getElementById("demo").innerHTML = text3;
+}
